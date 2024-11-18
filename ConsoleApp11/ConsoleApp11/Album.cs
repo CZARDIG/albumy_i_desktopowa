0a85253370241a9011c5b642ef4dfc6a08d9c0a3
CZARDIG <67341998+CZARDIG@users.noreply.github.com>
2024-11-18 11:53:25 +0100

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/Album.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    public class Album
+    {
+        public string artist { get; set; }
+        public string album { get; set; }
+        public string songNumber { get; set; }
+        public string year { get; set; }
+        public string downloadNumber { get; set; }
+
+    }
+}
---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Album> albums = new List<Album>();
+
+            string path = @"C:\Users\TEB\Downloads\pliki2\Data.txt";
+            StreamReader stream = new StreamReader(path, Encoding.UTF8, true);
+
+            while(!stream.EndOfStream)
+            {
+                albums.Add(new Album() { 
+                    artist = stream.ReadLine(),
+                    album = stream.ReadLine(),
+                    songNumber = stream.ReadLine(),
+                    year = stream.ReadLine(),
+                    downloadNumber = stream.ReadLine(),
+                });
+                stream.ReadLine();
+            }
+            stream.Close();
+
+            foreach(Album album in albums)
+            {
+                Console.WriteLine(album.artist);
+                Console.WriteLine(album.album);
+                Console.WriteLine(album.songNumber);
+                Console.WriteLine(album.year);
+                Console.WriteLine(album.downloadNumber);
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
